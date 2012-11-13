99afa9918a54d40a7a0790b5fbef7809b6d298f8
Néstor Alejandro Bermúdez <nestor.bermudez@unitec.edu>
2012-11-12 18:04:48 -0600

    Mas cosas

--- PaperGenerator2/src/java/BL/general/resumenBean.java
+++ PaperGenerator2/src/java/BL/general/resumenBean.java
@@ -14,10 +14,29 @@ import javax.faces.context.FacesContext;
  *
  * @author Alejandro
  */
-@ManagedBean(name="resumen")
+@ManagedBean(name = "resumen")
 @RequestScoped
 public class resumenBean {
+
     private Paper paper;
+    private String autoresString;
+    private String palabrasClaveString;
+
+    public String getPalabrasClaveString() {
+        return palabrasClaveString;
+    }
+
+    public void setPalabrasClaveString(String palabrasClaveString) {
+        this.palabrasClaveString = palabrasClaveString;
+    }
+
+    public String getAutoresString() {
+        return autoresString;
+    }
+
+    public void setAutoresString(String autoresString) {
+        this.autoresString = autoresString;
+    }
 
     public Paper getPaper() {
         return paper;
@@ -26,16 +45,31 @@ public class resumenBean {
     public void setPaper(Paper paper) {
         this.paper = paper;
     }
-    
-    public void setPaper(String titulo){
+
+    public void setPaper(String titulo) {
         this.paper = AppSingleton.getInstance().getPaper(titulo);
     }
-    
-    public resumenBean(){
+
+    public resumenBean() {
         FacesContext context = FacesContext.getCurrentInstance();
         Map<String, String> paramMap = context.getExternalContext().getRequestParameterMap();
         String doc = paramMap.get("doc");
         paper = AppSingleton.getInstance().getPaper(Integer.parseInt(doc));
         //AppSingleton.getInstance().addMessage(paper.getResumen());
+        autoresString = "";
+        for (String s : paper.getAutores()) {
+            autoresString += s + ", ";
+        }
+        if (autoresString.length() > 1) {
+            autoresString = autoresString.substring(0, autoresString.length() - 2) + ".";
+        }
+
+        palabrasClaveString = "";
+        for (String s : paper.getFrases_clave()) {
+            palabrasClaveString += s + ", ";
+        }
+        if (palabrasClaveString.length() > 1) {
+            palabrasClaveString = palabrasClaveString.substring(0, palabrasClaveString.length() - 2) + ".";
+        }
     }
 }
